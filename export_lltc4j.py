43aece3746e2e3fe464664229b5d2c1d454f308c
Thomas Schweizer <tschweiz@cs.washington.edu>
2023-09-27 16:37:56 -0700

    Cleanup

--- export_lltc4j.py
+++ export_lltc4j.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3
 
 """
-This script exports the VCS url, commit hash, and labelled lines from the LLTC4J
-dataset[1]. LLTC4J stands for Line-Labelled Tangled Commits for Java.
+This script exports the LLTC4J dataset from the SmartSHARK database to the disk as CSV files[1].
+LLTC4J stands for Line-Labelled Tangled Commits for Java.
+
+The following artifacts are exported:
+    - lltc4j-commits.csv contains the list of bug-fixing commits and the url of
+      the git repository.
+    - The ground truth for each commit in lltc4j-commits.csv. The ground truth
+      is a CSV file labelling each changed line in the commit with a group. Only
+      lines changing the code are included.
 
 The commit exported have the following properties:
 - The commit is labelled as a bugfix by developers and researchers.
 - The commit has only one parent. This is to avoid ambiguity where we don't know which parent was diffed against to manually label the lines.
+- The commit contains at least one code change. Documentation, tests, and whitespace related changes are ignored.
 
 References:
 1. Herbold, Steffen, et al. "A fine-grained data set and analysis of tangling in bug fixing commits." Empirical Software Engineering 27.6 (2022): 125.
 
-Command Line Args:
-- None
+Arguments:
+- --outdir. Specify the output directory for the script results 
+- The --projects argument is optional. By default, all projects are exported.
+- The --number argument is optional. By default, all commits are exported.
 
 Output:
 The VCS url, commit hash and labelled lines are outputed on stdout with the folowing
@@ -21,6 +31,7 @@ CSV format:
 - commit_hash: Hash of the commit.
 - ground_truth: The manual labelling for this commit.
 """
+
 import argparse
 import os
 import sys
@@ -35,10 +46,11 @@ from pycoshark.mongomodels import (
 )
 from pycoshark.utils import create_mongodb_uri_string
 from tqdm import tqdm
+from typing import List
 import pandas as pd
 
 PROJECTS = [
-    "Ant-ivy",
+    "ant-ivy",
     "archiva",
     "commons-bcel",
     "commons-beanutils",
@@ -86,7 +98,7 @@ LINE_LABELS_CODE_NO_FIX = ["refactoring", "unrelated", "no_bugfix"]
 
 def connect_to_db():
     """
-    Connect to the smartshark database or throws an error.
+    Connect to the SmartSHARK database or throws an error.
     """
     credentials = {
         "db_user": "",
@@ -99,6 +111,8 @@ def connect_to_db():
     uri = create_mongodb_uri_string(**credentials)
     connect("smartshark_2_2", host=uri, alias="default")
 
+    # Fail early in case the database doesn't exists. mongodb doesn't provide
+    # an API to test if the connection is established directly.
     if Project.objects(name="giraph").get():
         print("Connected to database", file=sys.stderr)
     else:
@@ -107,51 +121,19 @@ def connect_to_db():
         )
 
 
-def export_commit_hashes(args: argparse.Namespace):
+def label_lines(hunks: List[Hunk]) -> pd.DataFrame:
     """
-    Print the commits in the LLTC4J dataset on stdout.
-    The format is the following:
-    - project_name: Name of the project.
-    - vcs_url: URL of the VCS
-    - commit_hash: Hash of the commit.
-    - parent_hash: Hash of the parent commit.
-    """
-    print("project_name,vcs_url,commit_hash,parent_hash")
-
-    project_ids = []
-
-    for project in Project.objects(name__in=PROJECTS):
-        project_ids.append(project.id)
-
-    for vcs_system in VCSSystem.objects(
-        project_id__in=project_ids, repository_type="git"
-    ):
-        project = Project.objects(id=vcs_system.project_id).get()
-        for commit in Commit.objects(vcs_system_id=vcs_system.id):
-            if (
-                commit.labels is not None
-                and "validated_bugfix" in commit.labels
-                and commit.labels["validated_bugfix"]
-                and len(commit.parents) == 1
-            ):
-                print(
-                    f"{project.name},{vcs_system.url},{commit.revision_hash},{commit.parents[0]}"
-                )
-
-
-def export_ground_truth_for_hunks(hunks) -> pd.DataFrame:
-    """
-    Exports the ground truth for a file in CSV format.
+    Groups line changes into two groups for the given hunks. Only lines representing
+    changes in the code are included. Line changes for tests, documentation, and whitespace are ignored.
 
     Arguments:
-    - hunks: The hunks of the file.
+    - hunks: The hunks containing the lines to label
 
-    Returns: A list of the ground truth for each line in the file. The format is the following:
-    [{
-        source_line_number: The line number of in the old file.
-        target_line_number: The line number of in the new file.
-        label: The label of the line.
-    }, ...]
+    Returns:
+    A DataFrame with the following columns:
+    - source_line_number: The line number of in the old file.
+    - target_line_number: The line number of in the new file.
+    - label: The label of the line, either "bugfix" or "nonbugfix".
     """
     ground_truth = []
 
@@ -159,7 +141,7 @@ def export_ground_truth_for_hunks(hunks) -> pd.DataFrame:
         hunk_content_by_line = hunk.content.splitlines()
 
         for label, offset_line_numbers in hunk.lines_verified.items():
-            if (label not in LINE_LABELS_CODE):
+            if label not in LINE_LABELS_CODE:
                 continue
 
             for i in offset_line_numbers:
@@ -180,15 +162,20 @@ def export_ground_truth_for_hunks(hunks) -> pd.DataFrame:
                     group = "nonbugfix"
                 else:
                     group = label
-                
+
                 ground_truth.append((source_line_number, target_line_number, group))
-    return pd.DataFrame(ground_truth, columns=["source", "target", "group"]).astype({"source": "Int64", "target": "Int64"}, copy=False)
+    return pd.DataFrame(ground_truth, columns=["source", "target", "group"]).astype(
+        {"source": "Int64", "target": "Int64"}, copy=False
+    )
 
 
-def export_ground_truth_for_commit(commit):
+def export_ground_truth_for_commit(commit) -> pd.DataFrame:
     """
     Exports the ground truth for a commit.
-    file, source, target, group 
+    Only considerr
+    Filters out non-java files and Java test files.
+    file, source, target, group
+    Returns a dataframe or None if there were no relevant changes in the commit.
     """
     if (
         commit.labels is not None
@@ -209,10 +196,14 @@ def export_ground_truth_for_commit(commit):
                 # in our evaluation framework.
                 file = File.objects(id=fa.file_id).get()
 
-            if not file.path.endswith(".java") or file.path.endswith("Test.java") or "src/test" in file.path:
+            if (
+                not file.path.endswith(".java")
+                or file.path.endswith("Test.java")
+                or "src/test" in file.path
+            ):
                 continue
 
-            hunks_df = export_ground_truth_for_hunks(Hunk.objects(file_action_id=fa.id))
+            hunks_df = label_lines(Hunk.objects(file_action_id=fa.id))
             hunks_df["file"] = file.path
             file_frames.append(hunks_df)
         if len(file_frames) == 0:
@@ -220,12 +211,25 @@ def export_ground_truth_for_commit(commit):
         return pd.concat(file_frames)
 
 
-def export_ground_truth(args: argparse.Namespace):
+def export_lltc4j(out_dir: str, projects: List[str], number: int):
     """
-    Exports the ground truth from the LLTC4J dataset into CSV files. Results are
-    written in the directory specified by the --outdir argument. The ground truth
-    for each commit is store in a directory named after the project and the commit
-    hash. The ground truth is stored in a file named truth.csv.
+    Exports the LLTC4J dataset from its database to the disk as CSV files.
+    The following artifacts are exported:
+    - lltc4j-commits.csv contains the list of bug-fixing commits and the url of
+      the git repository.
+    - The ground truth for each commit in lltc4j-commits.csv. The ground truth
+      is a CSV file labelling each changed line in the commit with a group. Only
+      lines changeding the code are included.
+
+    lltc4j-commits.csv is written at the root of the directory specified by the --outdir argument.
+
+    The ground truth for each commit is stored in a file named truth.csv located in a directory named after the project and the commit
+    hash at the root of the directory specified by the --outdir argument.
+
+    The format of the lltc4j-commits.csv is the following:
+    - vcs_url: URL of the VCS
+    - commit_hash: Hash of the commit.
+    - parent_hash: Hash of the parent commit.
 
     The format of the truth.csv file is the following:
     - file: The path of the file.
@@ -234,74 +238,79 @@ def export_ground_truth(args: argparse.Namespace):
     - group: The label of the line.
 
     Arguments:
-    - args: The command line arguments. 
-            * The --outdir argument is required. 
-            * The --projects argument is optional. By default, all projects are exported.
-            * The --number argument is optional. By default, all commits are exported.
+    - outdir: Root directory where to store the exported files.
+    - projects: List of projects to include.
+    - number: Number of commits to include.
     """
-
-    out_dir = os.path.realpath(args.outdir)
-    if not os.path.exists(args.outdir):
-        raise ValueError(f"Directory {out_dir} does not exist.")
 
     early_exit = False
     counter = 0
-    for project in Project.objects(name__in=args.projects):
+    commits_hashes = []
+
+    for project in Project.objects(name__in=projects):
         print(f"Processing project {project.name}", file=sys.stderr)
         vcs_system = VCSSystem.objects(project_id=project.id).get()
-        for commit in tqdm(Commit.objects(vcs_system_id=vcs_system.id), desc='Commits'):
+        for commit in tqdm(Commit.objects(vcs_system_id=vcs_system.id), desc="Commits"):
             # Early exit if we have processed enough commits.
-            if args.number is not None and counter >= args.number:
+            if number is not None and counter >= number:
                 early_exit = True
                 break
 
             ground_truth_commit_frame = export_ground_truth_for_commit(commit)
-            if ground_truth_commit_frame is not None and len(ground_truth_commit_frame) > 0:
-                commit_dir = os.path.join(out_dir, f"{project.name}_{commit.revision_hash[:6]}")
+            if (
+                ground_truth_commit_frame is not None
+                and len(ground_truth_commit_frame) > 0
+            ):
+                # Reorder columns to be compatible with the rest of the pipeline.
+                ground_truth_commit_frame = ground_truth_commit_frame.reindex(
+                    columns=["file", "source", "target", "group"]
+                )
+
+                # Create directory for the commit to store results if it doesn't exist yet.
+                commit_dir = os.path.join(
+                    out_dir, f"{project.name}_{commit.revision_hash[:6]}"
+                )
                 os.makedirs(commit_dir, exist_ok=True)
 
+                # Export ground truth to CSV file.
                 ground_truth_file = os.path.join(commit_dir, "truth.csv")
-
-                # Reorder columns to be compatible with the rest of the pipeline.
-                ground_truth_commit_frame = ground_truth_commit_frame.reindex(columns=['file', 'source', 'target', 'group'])
                 ground_truth_commit_frame.to_csv(ground_truth_file, index=False)
 
+                commits_hashes.append(
+                    (vcs_system.url, commit.revision_hash, commit.parents[0])
+                )
                 counter += 1
-        
+
         if early_exit:
             break
+
+    commits_hashes_df = pd.DataFrame(
+        commits_hashes, columns=["vcs_url", "commit_hash", "parent_hash"]
+    )
+    commit_hashes_file = os.path.join(out_dir, "lltc4j-commits.csv")
+    commits_hashes_df.to_csv(commit_hashes_file, index=False)
+
     print(f"Processed {counter} commits.", file=sys.stderr)
+
 
 def main():
     """
     Implement the logic of the script. See the module docstring.
     """
-    main_parser = argparse.ArgumentParser(prog="export_lltc4j_utb.py")
-    subparsers = main_parser.add_subparsers(
-        title="subcommands",
-        help="LLTC4J dataset export commands",
-        required=True,
-        dest="subcommand",
+    main_parser = argparse.ArgumentParser(
+        prog="export_lltc4j_utb.py",
+        description="Exports the commit hashes and ground truth from the manually validated bug-fixes from the LLTC4J dataset",
     )
 
-    commits_parser = subparsers.add_parser(
-        "commits-hashes",
-        help="Exports the commit hashes of the manually validated bug-fixes from the LLTC4J dataset",
-    )
-    commits_parser.set_defaults(func=export_commit_hashes)
-
-    commits_parser = subparsers.add_parser(
-        "ground-truth",
-        help="Exports the ground truth of the manually validated bug-fixes from the LLTC4J dataset",
-    )
-    commits_parser.add_argument(
+    main_parser.add_argument(
         "-o",
         "--outdir",
-        help="The output directory where to write the ground truth CSV files.",
+        help="The output directory storing the commit list and the ground truth CSVs.",
         metavar="PATH",
         required=True,
     )
-    commits_parser.add_argument(
+
+    main_parser.add_argument(
         "-p",
         "--projects",
         help="The projects to export. By default, all projects are exported.",
@@ -309,7 +318,8 @@ def main():
         nargs="+",
         default=PROJECTS,
     )
-    commits_parser.add_argument(
+
+    main_parser.add_argument(
         "-n",
         "--number",
         help="The number of commits to export. By default, all commits are exported.",
@@ -317,12 +327,15 @@ def main():
         type=int,
         default=None,
     )
-    commits_parser.set_defaults(func=export_ground_truth)
 
     args = main_parser.parse_args()
 
+    out_dir = os.path.realpath(args.outdir)
+    if not os.path.exists(args.outdir):
+        raise ValueError(f"Directory {out_dir} does not exist.")
+
     connect_to_db()
-    args.func(args)
+    export_lltc4j(out_dir, args.projects, args.number)
 
 
 if __name__ == "__main__":
